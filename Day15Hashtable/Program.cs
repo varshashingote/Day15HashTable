5b20f05566741073d31c1f8c33f47fe8e2bb233b
varsha Gunjal <varshashingote01@gmail.com>
2023-02-13 13:46:45 +0530

    [varsha] Ability to find frequency of worlds in the sentance

---
+++ Day15Hashtable/Program.cs
@@ -0,0 +1,44 @@
+﻿using DatastructureDemo130Batch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Hashtable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph = "To be or not to be";
+            CountNumbOfOccurence(paragraph);
+            Console.ReadLine();
+        }
+
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToLower()))
+                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                else
+                    hashTabe.Add(word.ToLower(), 1); //to,1 
+            }
+            Console.WriteLine("Displaying after add operation");
+            hashTabe.Display();
+            string s = "or";
+            hashTabe.Remove(s);
+            Console.WriteLine("After removed an item {0}", s);
+            hashTabe.Display();
+        }
+    }
+}
+
+
+
+
